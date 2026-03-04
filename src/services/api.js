7b1780d123026220de8848dc934c7f690c82576d
Josias Silva <josilva1233@gmail.com>
2026-03-03 21:45:09 -0300

    asda

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,42 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://163.176.168.224'
+  // Garanta que o VITE_API_URL no seu .env seja http://163.176.168.224
+  baseURL: import.meta.env.VITE_API_URL 
 });
 
-// Interceptor para injetar o token em cada requisição
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem('@AxionID:token');
+  
   if (token) {
+    // Importante: O Sanctum espera 'Bearer ' antes do token
     config.headers.Authorization = `Bearer ${token}`;
   }
+  
+  config.headers.Accept = 'application/json';
+  config.headers['Content-Type'] = 'application/json';
+  
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
+
+api.interceptors.response.use(
+  (response) => response, 
+  (error) => {
+    // Se o token expirar ou for inválido (401), desloga o usuário
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('@AxionID:token');
+      localStorage.removeItem('@AxionID:role');
+      localStorage.removeItem('user_data');
+      
+      // Só redireciona se não estiver já na página de login
+      if (window.location.pathname !== '/') {
+        window.location.href = '/'; 
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
