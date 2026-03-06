93e88fd22cc5f9de719aadf288bb6b155a5f9743
Josias Silva <josilva1233@gmail.com>
2026-03-06 11:56:41 -0300

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import UserDetail from './pages/UserDetail';
-import ForgotPassword from './pages/ForgotPassword'; // <-- 1. IMPORTAR O NOVO COMPONENTE
+import MyProfile from './pages/MyProfile'; // <-- ADICIONADO
+import ForgotPassword from './pages/ForgotPassword'; 
 import { ProtectedRoute } from './components/ProtectedRoute'; 
 import CompleteProfile from './pages/CompleteProfile';
 import './App.css';
@@ -15,40 +16,29 @@ function App() {
         {/* Rotas Públicas */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        
-        {/* 2. ADICIONAR ROTA DE RECUPERAÇÃO DE SENHA */}
         <Route path="/forgot-password" element={<ForgotPassword />} />
-
         <Route path="/" element={<Navigate to="/login" replace />} />
 
-        {/* Rota de Completar Perfil */}
+        {/* Rotas Protegidas (Exigem Login) */}
         <Route 
           path="/complete-profile" 
-          element={
-            <ProtectedRoute>
-              <CompleteProfile />
-            </ProtectedRoute>
-          } 
+          element={<ProtectedRoute><CompleteProfile /></ProtectedRoute>} 
         />
 
-        {/* Dashboard Protegido */}
         <Route 
           path="/dashboard" 
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } 
+          element={<ProtectedRoute><Dashboard /></ProtectedRoute>} 
         />
 
-        {/* Detalhes do Usuário */}
+        {/* Nova Rota: Meu Perfil */}
+        <Route 
+          path="/profile" 
+          element={<ProtectedRoute><MyProfile /></ProtectedRoute>} 
+        />
+
         <Route 
           path="/dashboard/user/:id" 
-          element={
-            <ProtectedRoute>
-              <UserDetail />
-            </ProtectedRoute>
-          } 
+          element={<ProtectedRoute><UserDetail /></ProtectedRoute>} 
         />
 
         {/* Fallback Global */}
