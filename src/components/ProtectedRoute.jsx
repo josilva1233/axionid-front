b4ff7e443d080ab9cd2a1c2019c51d02de24040e
Josias Silva <josilva1233@gmail.com>
2026-03-06 16:39:18 -0300

    ads

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 export const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("@AxionID:token");
 
-  // Se não houver token, redireciona para o login na raiz
+  // Se não houver token, redireciona pa
   if (!token) {
     return <Navigate to="/" replace />;
   }
