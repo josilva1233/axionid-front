9134ebd841f074d1b1bc42d48bd5ec46f8c1f499
Josias Silva <josilva1233@gmail.com>
2026-03-06 12:06:19 -0300

    ar

--- src/components/dashboard/UserDropdown.jsx
+++ src/components/dashboard/UserDropdown.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../services/api";
+import api from "../../services/api";
 
 export default function MyProfile() {
   const [user, setUser] = useState(null);
