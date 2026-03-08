5b90049780219abfa58a3fcfc5c9decb4cdeb471
Josias Silva <josilva1233@gmail.com>
2026-03-08 14:28:48 -0300

    Explique aqui o que você alterou no código

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-// REMOVIDO: import UserDetail from './pages/UserDetail'; 
 import ForgotPassword from './pages/ForgotPassword'; 
 import { ProtectedRoute } from './components/ProtectedRoute'; 
 import CompleteProfile from './pages/CompleteProfile';
