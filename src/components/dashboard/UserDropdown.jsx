a5d5fd887f5992777325f3a8e57bc89fe437a369
Josias Silva <josilva1233@gmail.com>
2026-06-26 20:28:15 -0300

    erro imports

--- src/components/dashboard/UserDropdown.jsx
+++ src/components/dashboard/UserDropdown.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-import '../Identidade.css';
+import '../../Identidade.css';
 
 const UserDropdown = ({ user, onLogout }) => {
   const [isOpen, setIsOpen] = useState(false);
