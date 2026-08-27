ec3f5b1e211f74e1b5ec5964112ee7fc9c173bd3
Josias Silva <josilva1233@gmail.com>
2026-08-27 20:59:36 -0300

    1231

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite' // ← ADICIONE ESTA LINHA
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss(), // ← ADICIONE ESTA LINHA
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
