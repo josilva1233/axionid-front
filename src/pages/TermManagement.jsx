e0f28a9dc96b606b7ca88c9d050d7b5f0d40b218
Josias Silva <josilva1233@gmail.com>
2026-08-31 16:02:07 -0300

    3123122222

--- src/pages/TermManagement.jsx
+++ src/pages/TermManagement.jsx
@@ -1,4 +1,4 @@
-// src/pages/TermManagement.jsx//teste
+// src/pages/TermManagement.jsx
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../services/api';
