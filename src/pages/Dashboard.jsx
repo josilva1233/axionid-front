fad884baea9bfa423a832bd6769496b6a8263a64
Josias Silva <josilva1233@gmail.com>
2026-09-01 14:16:52 -0300

    update filtros

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1073,6 +1073,7 @@ export default function Dashboard() {
                   ) : (
                     <TermManagement 
                       onViewUsers={handleViewTermAcceptances}
+                      ilters={filters}
                     />
                   )}
                 </div>
